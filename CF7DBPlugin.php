572eb1392879c24a694bd780b8c746e3e5cc51f5
Michael Simpson <michael.d.simpson@gmail.com>
2017-09-17 23:31:22 -0400

    Fixing sometimes-missing Submitted From ip address

--- CF7DBPlugin.php
+++ CF7DBPlugin.php
@@ -1539,16 +1539,20 @@ class CF7DBPlugin extends CF7DBPluginLifeCycle implements CFDBDateFormatter {
 
     // http://stackoverflow.com/questions/1634782/what-is-the-most-accurate-way-to-retrieve-a-users-correct-ip-address-in-php
     public function getIPAddress() {
+        $maybeIp = '';
         foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
             if (array_key_exists($key, $_SERVER) === true) {
                 foreach (explode(',', $_SERVER[$key]) as $ip) {
                     $ip = trim($ip); // just to be safe
                     if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
                         return $ip;
+                    } else if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
+                        $maybeIp = $ip;
                     }
                 }
             }
         }
+        return $maybeIp;
     }
 
 }
